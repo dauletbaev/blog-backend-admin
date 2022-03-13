9a0261aeb3369501b0f8a47331456da06b610754
Abat Dauletbaev <abat.dauletbayev@gmail.com>
2022-03-13 20:32:48 +0500

    dotenv error fixed

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
-dotenv.config();
+
+// Configure dotenv when developement environment
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
-
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 supabase.auth.setAuth(process.env.SUPABASE_API_KEY!);
